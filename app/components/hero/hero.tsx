ab783e02b0531c172cdc6faa1e2f016189185a4a
Nguyễn Bùi Mẫn Nhi <nhinbm.dev@gmail.com>
2025-04-30 22:10:19 +0700

    ui: home - about section

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -3,7 +3,7 @@ import HeroContents from "./hero-contents";
 
 const Hero: React.FC = () => {
   return (
-    <section className="overflow-hidden px-8 py-24 md:px-12 md:py-32 h-screen">
+    <section className="overflow-hidden h-screen">
       <div className="relative mx-auto max-w-5xl grid grid-cols-1 md:grid-cols-3 items-center h-full items-center">
         <div className="pointer-events-none relative z-5 md:col-span-2">
           <HeroContents />
